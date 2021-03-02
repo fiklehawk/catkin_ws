6d56478a2a2b5622e38be695979e6b98b8e26343
Aanish Tahir <aanishtahir19@gmail.com>
2021-03-02 19:09:58 +0500

    created a launch file for publisher subscriber

--- src/testing/scripts/sensor_subscriber.py
+++ src/testing/scripts/sensor_subscriber.py
@@ -9,22 +9,23 @@ class Subscriber:
     def Callback(self,data):
         self.distance = data.distance
         self.objectName = data.objectName
-        # rospy.loginfo('Distance reading from the %s is: %s'%(data.objectName, data.distance))
+        rospy.loginfo('Distance reading from the %s is: %s'%(data.objectName, data.distance))
     
     def readData(self):
         rospy.init_node('Sensof_info_Subscriber', anonymous=False)
         rospy.Subscriber('sensor_info', sensor_info, self.Callback)
-        # rospy.spin()
-        rospy.sleep(0.5)
-        return [self.distance, self.objectName]
+        rospy.spin()
+        # rospy.sleep(0.5)
+        # return [self.distance, self.objectName]
 
 if __name__ == '__main__':
     sn = Subscriber()
-    while True:
-        try:
-            d, n = sn.readData()
+    sn.readData()
+    # while True:
+    #     try:
+    #         d, n = sn.readData()
 
-            print("Distance from %s is %s"%(n,d))
-            break
-        except AttributeError:
-            print("Attribute Error")
+    #         print("Distance from %s is %s"%(n,d))
+    #         break
+    #     except AttributeError:
+    #         print("Attribute Error")
